c7378d11427ac84bf583ec2d4ebf87fa674c481e
Liam Watson <you@example.com>
2021-08-14 09:11:53 +0200

    dist

--- src/RunResourceCollect.java
+++ src/RunResourceCollect.java
@@ -1,3 +1,5 @@
+import Math;
+
 public class RunResourceCollect {
     public static void main(String[] args) {
         try{
@@ -5,5 +7,9 @@ public class RunResourceCollect {
         }catch (Exception e){
             System.out.println("file not found");
         }
+    }
+
+    public int dist(int x, int y, int z){
+        return Math.round(Math.sqrt(x*x + y*y + z*z));
     }
 }
