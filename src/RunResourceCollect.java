bbb8e1ccea93dd59075419b78cfc24c0dfb06870
Liam Watson <you@example.com>
2021-08-14 09:23:50 +0200

    file reading

--- src/RunResourceCollect.java
+++ src/RunResourceCollect.java
@@ -1,9 +1,32 @@
 import Math;
+import java.util.*;
+import java.io.*;
+
 
 public class RunResourceCollect {
     public static void main(String[] args) {
         try{
             Scanner scFile = new Scanner(new File(""));
+            //LINE ONE
+            String [] lineOne = scFile.nextLine().split(".");
+            int UR = Integer.parseInt(lineOne[0]);
+            int numShips = Integer.parseInt(lineOne[1]);
+            int shipCap = Integer.parseInt(lineOne[2]);
+            int numLab = Integer.parseInt(lineOne[3]);
+            int outpostThreshold = Integer.parseInt(lineOne[4]); //T - Outpost Processed Material Threshold (0 < T <= 100000)
+            int NQ = Integer.parseInt(lineOne[5]); //NQ - Number of Quotas (0 < NQ <= 10)
+
+            //LINE TWO
+            String [] lineTwo = scFile.nextLine().split("|");
+            String len = lineTwo.length;
+            
+            String resourceID = lineTwo[0];
+            String
+            while(scFile.hasNext()){
+                String [] line = scFile.nextLine("|");
+
+
+            }
         }catch (Exception e){
             System.out.println("file not found");
         }
